1e69f071258cc25adeacbc747cd59d5f62805f6b
aloko00 <aloka.studiomada@gmail.com>
2024-01-30 00:35:16 +0300

    Update view.sql

--- src/data/view.sql
+++ src/data/view.sql
@@ -31,12 +31,3 @@ create or replace view stat_parcelle as
 select (count(id_parcelle)*100/(select count(*) from v_information_parcelle_par_terrain)) as stat_parcelle,id_terrain  
 from v_information_parcelle_par_terrain group by id_terrain;
 
-
-
-drop view v_information_parcelle_par_terrain;
-drop view v_prix_rendement_prevision;
-drop view v_sum_prix_rendement_prevision;
- drop view v_rendement_par_qte;
- drop view v_prix_rendement_reel
- drop view v_sum_prix_rendement_reel
- drop view stat_parcelle;
